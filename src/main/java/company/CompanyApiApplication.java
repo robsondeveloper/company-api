932c4515f65161752b5aa000d907ccbcdab59e47
Robson Oliveira Da Costa <robsondeveloper@gmail.com>
2020-06-14 14:49:28 -0400

    init

---
+++ src/main/java/company/CompanyApiApplication.java
@@ -0,0 +1,13 @@
+package company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyApiApplication.class, args);
+	}
+
+}
